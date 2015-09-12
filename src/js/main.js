032c8e0756a4a176a664200467fec0fdb25f86c7
Joscha Holzhaeuer <j.holzhaeuerapps@outlook.com>
2015-09-12 21:34:23 +0200

    styled mobile project, improved margins and paddings

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,11 @@ $(document).ready(function($) {
     var $backToTop = $('.cd-top'),
         offset = ( $('.project-grid').offset().top ) + 100;
 
+    //fadeInSection();
+
     $(window).on('scroll', function() {
         backToTop();
+        //fadeInElement();
     });
 
     function backToTop() {
@@ -19,6 +22,27 @@ $(document).ready(function($) {
         } else {
             $backToTop.addClass('is-invisible');
         }
+    }
+
+    function fadeInSection() {
+        $('.element-fade-now').animate({
+                    'opacity': '1'
+                }, 600);
+    }
+
+    function fadeInElement() {
+        $('.element-fade').each(function () {
+
+            var offsetOfObject = $(this).offset().top;
+            var offsetOfWindow = $(window).scrollTop() + $(window).height()*(2/3);
+
+            if (offsetOfWindow > offsetOfObject) {
+
+                $(this).animate({
+                    'opacity': '1'
+                }, 600);
+            }
+        });
     }
 
     //smooth scroll to top
